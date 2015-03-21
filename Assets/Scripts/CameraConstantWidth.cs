7ffa0a0d4e526abcea414a349f32001b60150f97
Momin Khan <foolmoron@gmail.com>
2015-03-21 17:41:31 -0400

    Some physics/camera stuff

---
+++ Assets/Scripts/CameraConstantWidth.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class CameraConstantWidth : MonoBehaviour {
+
+    void Start() {
+        GetComponent<Camera>().orthographicSize = (Screen.currentResolution.height / 200f);
+    }
+}
