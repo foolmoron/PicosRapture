ee91c9628919d55cece1f49da4ecfd838c5ed32b
Momin Khan <foolmoron@gmail.com>
2015-03-22 17:34:25 -0400

    Improved camera sizing and added camera target

--- Assets/Scripts/CameraConstantWidth.cs
+++ Assets/Scripts/CameraConstantWidth.cs
@@ -15,6 +15,8 @@ public class CameraConstantWidth : MonoBehaviour {
     void Update() {
         var defaultRatio = DefaultSize.x / DefaultSize.y;
         var screenRatio = (float)Screen.width / Screen.height;
-        (camera ?? GetComponent<Camera>()).orthographicSize = (DefaultSize.y / 200) * (defaultRatio / screenRatio);
+        var newSize = (DefaultSize.y / 200) * (defaultRatio / screenRatio);
+        var roundedSize = (float)System.Math.Round(newSize - 0.05, 2); // precision errors in multiplying can make the size a little off, so floor to 2nd decimal point
+        (camera ?? GetComponent<Camera>()).orthographicSize = roundedSize;
     }
 }
