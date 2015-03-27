0c18b3419f60d881d6a5172f951bb83ebf3a06f2
Momin Khan <foolmoron@gmail.com>
2015-03-26 20:52:43 -0400

    Frictionless sides, auto fire mode, mode toggles

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -19,6 +19,16 @@ public class Player : MonoBehaviour {
     public float ExplosionForce = 1000;
 
     public bool ScreenRelativeControls;
+    public bool AutoFireControls;
+
+    [Range(0f, 1f)]
+    public float AutoFireTimerMax = 0.2f;
+    [Range(0f, 1f)]
+    public float AutoFireTimer;
+    [Range(0f, 1f)]
+    public float ShootInterval = 0.1f;
+    [Range(0f, 1f)]
+    public float TimeToShoot;
 
     new Rigidbody2D rigidbody2D;
 
@@ -27,25 +37,67 @@ public class Player : MonoBehaviour {
     }
 
     void Update() {
+        // debug toggles
+        if (Input.GetKeyDown(KeyCode.R)) {
+            ScreenRelativeControls = !ScreenRelativeControls;
+        } else if (Input.GetKeyDown(KeyCode.F)) {
+            AutoFireControls = !AutoFireControls;
+        }
         // handle shooting bullet
-        if (Input.GetMouseButtonDown(0)) {
-            var newBulletObj = (GameObject) Instantiate(BulletPrefab, transform.position, Quaternion.identity);
-
-            Vector2 shootDirection;
-            if (ScreenRelativeControls) {
-                shootDirection = (Camera.main.ScreenToViewportPoint(Input.mousePosition).to2() - (0.5f * Vector2.one)).normalized; // dir from center of screen to click
-                shootDirection = shootDirection.withY(-Mathf.Abs(shootDirection.y)); // pretend click is always in bottom half of screen
-                shootDirection = shootDirection.withY(Mathf.Clamp(shootDirection.y, -0.5f, -1f)).normalized; // pretend click is always far enough towards bottom of screen
-            } else {
-                shootDirection = (Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position)).normalized;
+        if (AutoFireControls) {
+            if (Input.GetMouseButton(0) && AutoFireTimer <= 0) { // fire instantly on click if you're not already in auto fire mode
+                TimeToShoot = 0;
+            } else if (Input.GetMouseButtonUp(0)) { // immediately turn off auto fire when letting go
+                AutoFireTimer = 0;
             }
-            newBulletObj.GetComponent<Rigidbody2D>().velocity = shootDirection * BulletSpeed;
 
-            rigidbody2D.AddForce(new Vector2(-shootDirection.x * ShootHorizontalForce, 0));
-            if (rigidbody2D.velocity.y < 0 && shootDirection.y < 0) { // hover a bit 
-                rigidbody2D.velocity = rigidbody2D.velocity.withY(ShootHoverVelocity);
-            } else if (shootDirection.y > 0) {
-                rigidbody2D.velocity = rigidbody2D.velocity.plusY(-shootDirection.y * ShootDiveForce);
+            if (Input.GetMouseButton(0)) {
+                AutoFireTimer = AutoFireTimerMax;
+            }
+            if (AutoFireTimer > 0) {
+                AutoFireTimer -= Time.deltaTime;
+                TimeToShoot -= Time.deltaTime;
+                if (TimeToShoot <= 0) {
+                    // fire
+                    {
+                        var newBulletObj = (GameObject) Instantiate(BulletPrefab, transform.position, Quaternion.identity);
+
+                        Vector2 shootDirection;
+                        if (ScreenRelativeControls) {
+                            shootDirection = (Camera.main.ScreenToViewportPoint(Input.mousePosition).to2() - (0.5f * Vector2.one)).normalized; // dir from center of screen to click
+                            shootDirection = shootDirection.withY(-Mathf.Abs(shootDirection.y)); // pretend click is always in bottom half of screen
+                            shootDirection = shootDirection.withY(Mathf.Clamp(shootDirection.y, -0.5f, -1f)).normalized; // pretend click is always far enough towards bottom of screen
+                        } else {
+                            shootDirection = (Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position)).normalized;
+                        }
+                        newBulletObj.GetComponent<Rigidbody2D>().velocity = shootDirection * BulletSpeed;
+
+                        rigidbody2D.AddForce(new Vector2(-shootDirection.x * ShootHorizontalForce, 0));
+                        rigidbody2D.velocity = rigidbody2D.velocity.withY(Mathf.Max(ShootHoverVelocity, rigidbody2D.velocity.y)); // hover a bit 
+                    }
+                    TimeToShoot += ShootInterval;
+                }
+            }
+        } else {
+            if (Input.GetMouseButtonDown(0)) {
+                var newBulletObj = (GameObject) Instantiate(BulletPrefab, transform.position, Quaternion.identity);
+
+                Vector2 shootDirection;
+                if (ScreenRelativeControls) {
+                    shootDirection = (Camera.main.ScreenToViewportPoint(Input.mousePosition).to2() - (0.5f * Vector2.one)).normalized; // dir from center of screen to click
+                    shootDirection = shootDirection.withY(-Mathf.Abs(shootDirection.y)); // pretend click is always in bottom half of screen
+                    shootDirection = shootDirection.withY(Mathf.Clamp(shootDirection.y, -0.5f, -1f)).normalized; // pretend click is always far enough towards bottom of screen
+                } else {
+                    shootDirection = (Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position)).normalized;
+                }
+                newBulletObj.GetComponent<Rigidbody2D>().velocity = shootDirection * BulletSpeed;
+
+                rigidbody2D.AddForce(new Vector2(-shootDirection.x * ShootHorizontalForce, 0));
+                if (rigidbody2D.velocity.y < 0 && shootDirection.y < 0) { // hover a bit 
+                    rigidbody2D.velocity = rigidbody2D.velocity.withY(ShootHoverVelocity);
+                } else if (shootDirection.y > 0) {
+                    rigidbody2D.velocity = rigidbody2D.velocity.plusY(-shootDirection.y * ShootDiveForce);
+                }
             }
         }
     }
---
+++ Assets/Scripts/PlayerModeTracker.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerModeTracker : MonoBehaviour {
+
+    Player player;
+    Text text;
+
+    void Start() {
+        player = FindObjectOfType<Player>();
+        text = GetComponent<Text>();
+    }
+
+    void Update() {
+        text.text = ((player.AutoFireControls) ? "auto" : "semi") + "-" + ((player.ScreenRelativeControls) ? "relative" : "absolute");
+    }
+}
