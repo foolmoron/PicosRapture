0ee7027561df5e107eb8c8b1dc5265164c5a7328
Momin Khan <foolmoron@gmail.com>
2015-03-28 19:30:42 -0400

    Added Nene and Darnell and cycling characters

---
+++ Assets/Scripts/CyclePlayerPack.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+public class CyclePlayerPack : MonoBehaviour {
+
+    PlayerPacks packs;
+    Text text;
+
+    void Start() {
+        packs = FindObjectOfType<PlayerPacks>();
+        text = GetComponentInChildren<Text>();
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.R))
+            Cycle();
+    }
+
+    public void Cycle() {
+        packs.CyclePlayerPack();
+        var pack = packs.Packs[packs.CurrentPackIndex];
+        text.text = "Character: " + pack.name.ToUpper() + " (press here or R to cycle)";
+    }
+}
---
+++ Assets/Scripts/PlayerPacks.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+using UnityEditor;
+
+using UnityEngine;
+using System.Collections;
+
+[Serializable]
+public class PlayerPack {
+    public string name = "N/A";
+    public Color HairColor = Color.white;
+    public Color FaceColor = Color.white;
+    public Color BodyColor = Color.white;
+    public Color FeetColor = Color.white;
+    public Sprite WeaponSprite;
+    public GameObject BulletPrefab;
+}
+[ExecuteInEditMode]
+public class PlayerPacks : MonoBehaviour {
+
+    public PlayerPack[] Packs;
+    public int CurrentPackIndex = 0;
+    int previousIndex;
+
+    //Player player;
+    //SpriteRenderer hair;
+    //SpriteRenderer face;
+    //SpriteRenderer body;
+    //SpriteRenderer feet;
+    //SpriteRenderer weapon;
+
+    void Start() {
+        SelectPlayerPack(0);
+    }
+
+    void Update() {
+        if (CurrentPackIndex != previousIndex) {
+            SelectPlayerPack(CurrentPackIndex);
+        }
+    }
+
+    public void CyclePlayerPack() {
+        SelectPlayerPack(CurrentPackIndex + 1);
+    }
+
+    public void SelectPlayerPack(int index) {
+        CurrentPackIndex = ((index % Packs.Length) + Packs.Length) % Packs.Length;
+        previousIndex = CurrentPackIndex;
+        var pack = Packs[CurrentPackIndex];
+
+        var player = GetComponent<Player>();
+        var hair = player.transform.FindChild("Sprite/Hair").GetComponent<SpriteRenderer>();
+        var face = player.transform.FindChild("Sprite/Face").GetComponent<SpriteRenderer>();
+        var body = player.transform.FindChild("Sprite/Body").GetComponent<SpriteRenderer>();
+        var feet = player.transform.FindChild("Sprite/Feet").GetComponent<SpriteRenderer>();
+        var weapon = player.transform.FindChild("WeaponRoot/Weapon").GetComponent<SpriteRenderer>();
+
+        player.BulletPrefab = pack.BulletPrefab;
+        hair.color = pack.HairColor;
+        face.color = pack.FaceColor;
+        body.color = pack.BodyColor;
+        feet.color = pack.FeetColor;
+        weapon.sprite = pack.WeaponSprite;
+    }
+}
