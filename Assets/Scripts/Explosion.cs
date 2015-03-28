93631e93c3f4049301ae7dc05d9b8be95fd6a2e9
Momin Khan <foolmoron@gmail.com>
2015-03-28 17:42:47 -0400

    Better explosion anim

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -2,30 +2,6 @@
 using System.Collections;
 
 public class Explosion : MonoBehaviour {
-
-    [Range(1, 60)]
-    public int DestroyFrameDelay = 6;
-    [Range(1, 60)]
-    public int FlashDelay = 3;
-    int frames;
-
-    SpriteRenderer spriteRenderer;
-
-    void Start() {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-    }
-
-    void Update() {
-        if (frames < DestroyFrameDelay) {
-            if (frames >= FlashDelay) {
-                spriteRenderer.color = Color.black;
-            }
-            frames++;
-        } else {
-            Destroy(gameObject);
-        }
-    }
-
     void OnTriggerEnter2D(Collider2D other) {
         var player = other.GetComponent<Player>();
         if (player) {
