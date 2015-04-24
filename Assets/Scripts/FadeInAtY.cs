937f0d71c6ae08b26dd748ff2266e4947ecde3da
Momin Khan <foolmoron@gmail.com>
2015-04-23 22:42:06 -0400

    Added new clouds

---
+++ Assets/Scripts/FadeInAtY.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class FadeInAtY : MonoBehaviour {
+
+    public float FadeInY;
+    [Range(0.001f, 20)]
+    public float FadeInPeriod = 3;
+    SpriteRenderer spriteRenderer;
+
+    void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update() {
+        var currentY = transform.position.y;
+        var normalizedAmountAboveFadePoint = (currentY - FadeInY) / FadeInPeriod;
+        spriteRenderer.color = spriteRenderer.color.withAlpha(Mathf.Clamp01(normalizedAmountAboveFadePoint));
+    }
+}
