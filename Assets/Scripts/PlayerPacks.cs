0ebaa72ad39bdccc0158655ef37aa1b8fdf15227
Momin Khan <foolmoron@gmail.com>
2017-01-20 18:22:05 -0500

    Done

--- Assets/Scripts/PlayerPacks.cs
+++ Assets/Scripts/PlayerPacks.cs
@@ -25,7 +25,7 @@ public class PlayerPacks : MonoBehaviour {
         //PlayerPrefs.DeleteAll(); // for debug
         PlayerPrefs.SetInt(Packs[0].name, 1);
         for (int i = 0; i < Packs.Length; i++) {
-            Packs[i].Unlocked = true;
+            Packs[i].Unlocked = PlayerPrefs.GetInt(Packs[i].name, 0) != 0;
         }
         if (Application.isPlaying) {
             gameObject.SetActive(false);
